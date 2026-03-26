eb5ba4f07e6c1e37d009b5679721e04268d468be
Nadya Athalia Diva <nadyaathaliadiva@gmail.com>
2026-03-26 13:28:04 +0700

    simpan tugas pertemuan 1 sampai 5

--- pertemuan 5/pertemuan 5, dictionary.py
+++ pertemuan 5/pertemuan 5, dictionary.py
@@ -1,11 +1,11 @@
 #membuat struktur data dictionary
-userlogin ={"name":"Budi","age":30, "Role":"admin"}
+userlogin ={"name":"Nadya cakep banget","age":23, "Role":"penguasa dunia"}
 print(type(userlogin));
 
 #mengakses data dalam dictionary
 print(f"Nama Akun : {userlogin['name']}")
-print(f"Umur : {userlogin['age']} tahun")
-print(f"Role : {userlogin['Role']}")
+print(f"Umur Akun : {userlogin['age']} tahun")
+print(f"Role Akun : {userlogin['Role']}")
 
 #akses data seluruh
 print(userlogin.items())
@@ -13,21 +13,21 @@ print(userlogin.keys())
 print(userlogin.values())
 
 #menambahkan data baru dalam dictionary
-userlogin["email"] = "example@mail.com"
+userlogin["email"] = "nadyacakep@gmail.com"
 print(userlogin)
 
 #menghapus data big_On dari dictionary
-del userlogin["Role"]
+userlogin.pop("email")
 print(userlogin)
 
 #mengubah data kedalam dictionari big-O 0(1)
-userlogin["Role"] = "user"
+userlogin["name"] = "user"
 print(userlogin)
 
 #nested dictionary
-dbUser = {"user1":{"name":"Budi","age":30, "Role":"admin"},
-"user2":{"name":"Siti","age":25, "Role":"user"},
-"user3":{"name":"Andi","age":35, "Role":"guest"}}
+dbUser = {"user1":{"name":"Nadya","age":23, "Role":"ratu-atlantis"},
+"user2":{"name":"Linda","age":25, "Role":"anak baik"},
+"user3":{"name":"Rully","age":35, "Role":"anak jahat"}}
 
 print(dbUser)
 #akses value base key
